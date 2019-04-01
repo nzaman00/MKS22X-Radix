d3460d697315d0d929bde1b177ce2320100f42a5
nzaman00 <43052325+nzaman00@users.noreply.github.com>
2019-04-01 10:12:04 -0400

    Update Radix.java

--- Radix.java
+++ Radix.java
@@ -1,3 +1,7 @@
 public class Radix{
+  public static void radixsort(int[] data){
+    @SuppressWarnings("unchecked")
+    MyLinkedList<Integer>[] buckets = (MyLinkedList<Integer>[]) new Object[20];
+  }
 
 }
