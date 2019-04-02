9d0f769c424e1d8102787d0016ddb50a7b485dd4
nzaman00 <43052325+nzaman00@users.noreply.github.com>
2019-04-02 09:52:25 -0400

    Update Radix.java

--- Radix.java
+++ Radix.java
@@ -1,7 +1,19 @@
 public class Radix{
   public static void radixsort(int[] data){
     @SuppressWarnings("unchecked")
-    MyLinkedList<Integer>[] buckets = (MyLinkedList<Integer>[]) new Object[20];
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
+    for(int i =0; i < 20; i++){
+      buckets[i]= new MyLinkedList<Integer>();
+      }
+    for(int i =0;i < data.length;i++){
+      if(data[i]% 10 < 0){
+        buckets[9 + (data[i]% 10)].add(data[i]);
+      }
+      if(data[i]% 10 >= 0){
+        buckets[10 + (data[i]% 10)].add(data[i]);
+      }
+    }
+    
   }
 
 }
