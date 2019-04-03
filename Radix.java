6ee63ec8ef59216c71f6ef497897e9aaa4ffcede
nzaman00 <43052325+nzaman00@users.noreply.github.com>
2019-04-03 16:27:45 -0400

    Update Radix.java

--- Radix.java
+++ Radix.java
@@ -16,64 +16,75 @@ public class Radix{
     for(int i =0; i < 20; i++){
       buckets[i]= new MyLinkedList<Integer>();
       }
-    for(int Pow = 0; Pow < max; Pow++){
-      if(Pow == 0){//first case
-      if(data[i] < 0){
-        buckets[9 - ((Math.abs(data[i]) / (int)Math.pow(10, Pow))%10) ].add(data[i]);
-      }else{
-        buckets[10 + ((Math.abs(data[i]) / (int)Math.pow(10, Pow))%10)].add(data[i]);
+      for(int i = 0; i < data.length; i++){
+        int ones = data[i] % 10;
+        if (data[i] < 0){
+          buckets[9 - Math.abs(ones)].add(data[i]);
+        }else{
+          buckets[10 + ones].add(data[i]);
+        }
       }
-    }
-    for(int i = 0; i < 20; i++){
-      if(buckets[i].size() > 0){
-        Hold.extend(buckets[i]);
+      for(int i = 0; i < 20; i++){
+        if(buckets[i].size() > 0){
+          Hold.extend(buckets[i]);
+        }
       }
-    }
-  }
-  if (Pow + 1 == max){//final case
-    while(Hold.size() > 0){
-      int item = Hold.remove(0);
-      if(item < 0){
-        buckets[9 - ((Math.abs(item) / (int)Math.pow(10, Pow))%10) ].add(item);
-      }else{
-        buckets[ 10 + ((Math.abs(item) / (int)Math.pow(10, Pow))%10) ].add(item);
-      }
-    }
-    for(int i = 0; i < 20; i++){
-      if(buckets[i].size() > 0){
-        Hold.extend(buckets[i]);
-      }
-    }
-    for(int i = 0; i < Hold.size(); i++){
-      data[i] = Hold.get(i);
-    }
-  }
-  if(Pow > 0 && Pow+1 < max){//intermediate passes if any
-    while( Hold.size() > 0){
-      int item = Hold.remove(0);
-      if(item < 0){
-        buckets[9 - ((Math.abs(item) / (int)Math.pow(10, Pow))%10) ].add(item);
-      }else{
-        buckets[ 10 + ((Math.abs(item) / (int)Math.pow(10, Pow))%10) ].add(item);
-      }
-    }
-    Hold.clear();
-    for(int i = 0; i < 20; i++){
-      if(buckets[i].size() > 0){
-        Hold.extend(buckets[i]);
-      }
-    }
-  }
-  }
+      for(int place = 2; place <= max; place++){
+        while(Hold.size()> 0){
+          int item = Hold.remove(0);
+          int digitAt = (item / (int)Math.pow(10, place-1)) %10;
+          if(item < 0){
+            buckets[9 - Math.abs(digitAt)].add(item);
+          }else{
+              buckets[10 + digitAt].add(item);
+          }
+        }
+        for(int i = 0; i < 20; i++){
+          if(buckets[i].size() > 0){
+            Hold.extend(buckets[i]);
+          }
+        }
 
+      }
 
-
+      int index = 0;
+      while(Hold.size()>0){
+        data[index]= Hold.remove(0);
+        index++;
+      }
+    }
+    public static void main(String[]args){
+  System.out.println("Size\t\tMax Value\tquick/builtin ratio ");
+  int[]MAX_LIST = {1000000000,500,10};
+  for(int MAX : MAX_LIST){
+    for(int size = 31250; size < 2000001; size*=2){
+      long qtime=0;
+      long btime=0;
+      //average of 5 sorts.
+      for(int trial = 0 ; trial <=5; trial++){
+        int []data1 = new int[size];
+        int []data2 = new int[size];
+        for(int i = 0; i < data1.length; i++){
+          data1[i] = (int)(Math.random()*MAX);
+          data2[i] = data1[i];
+        }
+        long t1,t2;
+        t1 = System.currentTimeMillis();
+        Radix.radixsort(data2);
+        t2 = System.currentTimeMillis();
+        qtime += t2 - t1;
+        t1 = System.currentTimeMillis();
+        Arrays.sort(data1);
+        t2 = System.currentTimeMillis();
+        btime+= t2 - t1;
+        if(!Arrays.equals(data1,data2)){
+          System.out.println("FAIL TO SORT!");
+          System.exit(0);
+        }
+      }
+      System.out.println(size +"\t\t"+MAX+"\t"+1.0*qtime/btime);
+    }
+    System.out.println();
   }
-  public static void main(String[] args) {
-    int[] a1 = {1,22,12,74,44};
-    radixsort(a1);
-    System.out.println("radixsort(a1) : Should be [1, 22, 74, 312, 8444], Actual : " + Arrays.toString(a1) + "\n");
-
-  }
-
 }
+  }
